e218dee5501a4d2d16c3087e011c95f1ef29c002
ZarinaGuriyeva <123586934+ZarinaGuriyeva@users.noreply.github.com>
2023-03-05 22:09:08 +0100

    Add files via upload

---
+++ HWJava.java
@@ -0,0 +1,13 @@
+public class HWJava {
+    public void convertEuroToUsdVoid(double eur){
+        double result = eur*1.06;
+        System.out.println();
+
+    }
+
+    public double convertEuroToUsd(double eur){
+        double result = eur*1.06;
+        return 0;
+
+    }
+}
